996a3fea1456cb46fe2470791ee9827de0259c7b
Engity <ngtoan1107@gmail.com>
2022-07-26 11:45:19 -0700

    Apply Singleton Design for mainDisplay

--- src/TextBasedGUI_NavigationView.java
+++ src/TextBasedGUI_NavigationView.java
@@ -1,2 +1,17 @@
+import java.util.Scanner;
+
 public class TextBasedGUI_NavigationView {
+    /**
+     * The source to input from.
+     * Default is System.in
+     */
+
+    private static final Scanner INPUT_SCANNER = new Scanner (System.in);
+    private DungeonAdventure myGameController;
+    public TextBasedGUI_NavigationView(){
+        this(null);
+    }
+    public TextBasedGUI_NavigationView(final DungeonAdventure theGameController){
+        myGameController = theGameController;
+    }
 }
