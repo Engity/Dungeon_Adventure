516fc5b4db06ec650cc234a13d847920c11ca1da
Engity <ngtoan1107@gmail.com>
2022-08-07 13:44:21 -0700

    Add a fight interface for CombatView

---
+++ src/CombatController.java
@@ -0,0 +1,87 @@
+/**
+ * T CSS 360 A: Software Development And Quality Assurance Techniques
+ * Summer 2022
+ * Professor Tom Capaul
+ */
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * Keep the current status in a fight
+ * Control the player and monster during the fight
+ * {@code @author:} Toan Nguyen
+ * @version 08 07 2022
+ */
+public class CombatController {
+    private TextBasedGUI_CombatView myCombatView;
+    private int myCurrentTurnAmount;
+
+    private static DungeonAdventure myGameController;
+
+    static void attachController(final DungeonAdventure theGameController){
+        myGameController = theGameController;
+    }
+    private CombatController(){
+        myCurrentTurnAmount = 0;
+        myCombatView = TextBasedGUI_CombatView.getInstance();
+    }
+
+    private static final CombatController myCombatControllerInstance = new CombatController();
+
+    public static CombatController getInstance(){
+        return myCombatControllerInstance;
+    }
+
+    /**
+     * Initiate the fight
+     * Take in 2 dungeon characters, if one of them is hero, will ask the player for control
+     */
+    //Need other classes
+    void fight(){
+
+    }
+
+    /**
+     * Display a character's stat but in rectangle forms
+     * param a dungeon character
+     * Return a list of string with equal length
+     */
+
+    ArrayList<StringBuilder> parseDungeonCharacter(){
+        ArrayList<StringBuilder> res = new ArrayList<>();
+        int k = 0, longestWidth = 0;
+
+        //Basic example
+        String characterToSTR = """
+                Name: asDLK:l;asdlk;
+                Health: 90
+                Attack speed: 50
+                Dodge chance: 20
+                dsadasddsaasd
+                asdasdasddddddddddddddddddddddd
+                """;
+
+        Scanner scanline = new Scanner(characterToSTR);
+        
+        while (scanline.hasNext()){
+            //Init
+            StringBuilder line = new StringBuilder();
+            //Add to the string
+            line.append(scanline.nextLine());
+            //Increase the number of rows
+            longestWidth = Math.max(longestWidth, line.length());
+            res.add(line);
+            ++k;
+        }
+
+        //Add space to make every string has equal length
+        for (int i = 0 ; i < res.size(); i++){
+            res.get(i).append(" ".repeat(longestWidth - res.get(i).length()));
+        }
+
+
+        return res;
+    }
+
+}
